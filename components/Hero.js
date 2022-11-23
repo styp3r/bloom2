7f779edd5e9d76bfcd8bfbd352b9c5e8d7eda7c9
styp3r <stypertypes.com>
2022-11-23 19:04:27 +0530

    Creates Hero as a separate component

---
+++ components/Hero.js
@@ -0,0 +1,23 @@
+import styles from '../styles/Home.module.css'
+
+export default function Hero() {
+    return (
+        <div className={styles.hero}>
+            <div className={styles.heroCarousel}>
+                <div className={styles.heroSub1}>
+                </div>
+                <div className={styles.heroSub2}>
+                    <div className={styles.heroSub2Container1}>
+                        <div className={styles.heroBlock1}></div>
+                        <div className={styles.heroBlock2}></div>
+                    </div>
+                    <div className={styles.heroSub2Container2}>
+                        <div className={styles.heroBlock3}></div>
+                        <div className={styles.heroBlock4}></div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.trendingContainer}></div>
+        </div>
+    );
+}
--- components/MainFeed.js
+++ components/MainFeed.js
@@ -1,25 +1,10 @@
 import styles from '../styles/Home.module.css'
+import Hero from './Hero'
 
 export default function MainFeed() {
     return (
         <div className={styles.mainFeed}>
-            <div className={styles.hero}>
-                <div className={styles.heroCarousel}>
-                    <div className={styles.heroSub1}>
-                    </div>
-                    <div className={styles.heroSub2}>
-                        <div className={styles.heroSub2Container1}>
-                            <div className={styles.heroBlock1}></div>
-                            <div className={styles.heroBlock2}></div>
-                        </div>
-                        <div className={styles.heroSub2Container2}>
-                            <div className={styles.heroBlock3}></div>
-                            <div className={styles.heroBlock4}></div>
-                        </div>
-                    </div>
-                </div>
-                <div className={styles.trendingContainer}></div>
-            </div>
+            <Hero />
         </div>
     )
 }
