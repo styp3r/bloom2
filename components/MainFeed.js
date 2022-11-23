4d116b9bf18f8fbaa6f60b9b00eda11fb3df339d
styp3r <stypertypes.com>
2022-11-23 19:02:09 +0530

    Creates container layout for hero

--- components/MainFeed.js
+++ components/MainFeed.js
@@ -3,7 +3,23 @@ import styles from '../styles/Home.module.css'
 export default function MainFeed() {
     return (
         <div className={styles.mainFeed}>
-            <div></div>
+            <div className={styles.hero}>
+                <div className={styles.heroCarousel}>
+                    <div className={styles.heroSub1}>
+                    </div>
+                    <div className={styles.heroSub2}>
+                        <div className={styles.heroSub2Container1}>
+                            <div className={styles.heroBlock1}></div>
+                            <div className={styles.heroBlock2}></div>
+                        </div>
+                        <div className={styles.heroSub2Container2}>
+                            <div className={styles.heroBlock3}></div>
+                            <div className={styles.heroBlock4}></div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.trendingContainer}></div>
+            </div>
         </div>
     )
 }
