342d5a26ffbffd3850603168c5060bc25f60d524
styp3r <stypertypes.com>
2022-11-24 09:50:30 +0530

    Code refactor for topposts

--- components/GetTopPosts.js
+++ components/GetTopPosts.js
@@ -1,23 +1,30 @@
+//ER (Engagement Rank) = number of likes + number of comments
+
 const topPosts = [
     {
         id: 0,
-        title: "Post 1"
+        title: "Post 1",
+        ER: 0
     },
     {
         id: 1,
-        title: "Post 2"
+        title: "Post 2",
+        ER: 3
     },
     {
         id: 2,
-        title: "Post 3"
+        title: "Post 3",
+        ER: 2
     },
     {
         id: 3,
-        title: "Post 4"
+        title: "Post 4",
+        ER: 1
     },
     {
         id: 4,
-        title: "Post 5"
+        title: "Post 5",
+        ER: 4
     },
 
 ];
--- components/TopPosts.js
+++ components/TopPosts.js
@@ -2,13 +2,13 @@ import styles from '../styles/Home.module.css'
 import GetTopPosts from './GetTopPosts'
 import ArticleBox from './ArticleBox';
 
-export default function TopPosts(){
+export default function TopPosts() {
 
-    function displayTopPosts(getContent){
-        return <ArticleBox title={getContent.title}/>
+    function displayTopPosts(getContent) {
+        return <ArticleBox key={getContent.id} title={getContent.title} />
     }
 
-    return(
+    return (
         <div className={styles.topPosts}>
             {GetTopPosts.map(displayTopPosts)}
         </div>
